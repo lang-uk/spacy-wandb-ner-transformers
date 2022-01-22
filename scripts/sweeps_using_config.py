fd4f786308d9afc16a528787b153c7cd46995fe0
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2022-01-22 23:22:21 +0200

    Trying to unfuck the sweep training

--- scripts/sweeps_using_config.py
+++ scripts/sweeps_using_config.py
@@ -5,21 +5,32 @@ from spacy.training.initialize import init_nlp
 from spacy import util
 import spacy
 
+import yaml
 from thinc.api import Config
 import wandb
 
 
-def main(default_config: Path, output_path: Path):
+def main(default_config: Path, yaml_config: Path, output_path: Path, project_name=str):
+    def train_spacy():
+        loaded_local_config = util.load_config(default_config)
+        with wandb.init() as run:
+            spacy.prefer_gpu()
+
+            sweeps_config = Config(util.dot_to_dict(run.config))
+            merged_config = Config(loaded_local_config).merge(sweeps_config)
+
+            nlp = init_nlp(merged_config)
+            output_path.mkdir(parents=True, exist_ok=True)
+            train(nlp, output_path, use_gpu=True)
+
+    with open(yaml_config) as fp:
+        sweep_config = yaml.load(fp, Loader=yaml.SafeLoader)
+
     loaded_local_config = util.load_config(default_config)
-    with wandb.init() as run:
-        spacy.prefer_gpu()
-        sweeps_config = Config(util.dot_to_dict(run.config))
-        merged_config = Config(loaded_local_config).merge(sweeps_config)
-        nlp = init_nlp(merged_config)
-        output_path.mkdir(parents=True, exist_ok=True)
-        train(nlp, output_path, use_gpu=True)
+
+    sweep_id = wandb.sweep(sweep_config, project=loaded_local_config["variables"]["wandb_project_name"])
+    wandb.agent(sweep_id, train_spacy, count=20)
 
 
 if __name__ == "__main__":
     typer.run(main)
-
